96742d6abb9e44d1c2686c2bbb52cce3338bf0c4
dmytro <dmytro.zabolotnii@ut.ee>
2024-02-12 16:25:28 +0200

    ML-based pedestrian prediction prototype implementation

---
+++ nodes/detection/prediction/SGNet_sub.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import torch
+import os
+import os.path as osp
+from common_utils import color_points, calculate_danger_value
+from Net_sub import NetSubscriber
+from SGNet_utils import SGNet, parse_sgnet_args, sgnet_iter, SGNetDatasetInit
+
+
+class SGNetSubscriber(NetSubscriber):
+    def __init__(self, args):
+        super().__init__()
+        rospy.loginfo(self.__class__.__name__ + " - Initializing")
+        # this_dir = osp.dirname(__file__)
+        # model_name = args.model
+        # save_dir = osp.join(this_dir, 'checkpoints', args.dataset, model_name, str(args.dropout), str(args.seed))
+        # if not osp.isdir(save_dir):
+        #     os.makedirs(save_dir)
+
+        # os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+        self.args = args
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # utl.set_seed(int(args.seed))
+        self.model = SGNet(args)
+        self.inference_timer_duration = 0.5
+        self.pad_past = 8
+        self.future_horizon = 12
+        self.predictions_amount = 1
+        self.model = self.model.double().to(self.device)
+        print(rospy.get_param('~data_path_prediction') + args.checkpoint)
+        if osp.isfile(rospy.get_param('~data_path_prediction') + args.checkpoint):
+            self.checkpoint = torch.load(rospy.get_param('~data_path_prediction') + args.checkpoint, map_location=self.device)
+            self.model.load_state_dict(self.checkpoint['model_state_dict'])
+        # criterion = rmse_loss().to(device)
+
+        # test_gen = utl.build_data_loader(args, 'test', batch_size=1)
+        # print("Number of test samples:", test_gen.__len__())
+
+        # test
+        # test_loss, ADE_08, FDE_08, ADE_12, FDE_12 = test(model, test_gen, criterion, device)
+        self.inference_timer = rospy.Timer(rospy.Duration(self.inference_timer_duration), self.inference_callback)
+        print("Initializing the instance!")
+
+    def inference_callback(self, event):
+        if len(self.active_keys):
+            # Run inference
+            with self.lock:
+                temp_raw_trajectories = [self.all_raw_trajectories[key][:] for key in self.all_raw_trajectories if key in self.active_keys]
+                temp_velocities = [self.all_raw_velocities[key][:] for key in self.all_raw_velocities if key in self.active_keys]
+                temp_endpoints = [self.all_endpoints[key] for key in self.all_endpoints if key in self.active_keys]
+                temp_active_keys = set(self.active_keys)
+
+            inference_dataset = SGNetDatasetInit(temp_raw_trajectories, temp_velocities,
+                                                 end_points=temp_endpoints,
+                                                 pad_past=8,
+                                                 pad_future=0,
+                                                 inference_timer_duration=self.inference_timer_duration)
+
+            inference_result = sgnet_iter(inference_dataset, self.model, self.device, n=self.predictions_amount)
+            # print(len(inference_result))
+            # Update history of inferences
+            for j, _id in enumerate(temp_active_keys):
+                for i in range(len(inference_result)):
+                    self.all_predictions_history[_id][len(self.all_predictions_history[_id]) - 1]\
+                        .append(inference_result[i][j])
+                self.all_predictions_history[_id].append([])
+
+            # print('Timestamp:', event.current_real, 'Results:', inference_result, 'Endpoints:', self.all_endpoints)
+            # Update trajectory and process points
+            if self.self_traj_exists:
+                trajectory_length = self.velocity * self.inference_timer_duration * self.future_horizon
+                self.self_traj = self.self_new_traj.copy()
+                inference_colors = (list(map(color_points, inference_result, [self.self_traj] * len(inference_result),
+                                             [trajectory_length] * len(inference_result))))
+                # print('Inference_colors:', inference_colors)
+                endpoint_colors = color_points(inference_dataset.traj_flat[:, self.pad_past - 1],
+                                               self.self_traj, trajectory_length)
+                # print('Endpoint_colors:', endpoint_colors)
+                danger_values = np.zeros((len(endpoint_colors), len(inference_colors)))
+                for i in range(len(endpoint_colors)):
+                    for j in range(self.predictions_amount):
+                        danger_values[i, j] = calculate_danger_value(endpoint_colors[i], inference_colors[j][i])
+                # print(danger_values)
+                avg_danger_values = np.mean(danger_values, axis=1)
+
+                for j, _id in enumerate(temp_active_keys):
+                    self.all_predictions_history_danger_value[_id].append(avg_danger_values[j])
+                self.self_traj_history.append(self.self_traj[0])
+
+                # print('Danger_values:\n', avg_danger_values)
+                self.publish_markers(inference_dataset.traj_flat[:, self.pad_past - 1],
+                                     inference_result, inference_colors, endpoint_colors, avg_danger_values, self.predictions_amount)
+            self.move_endpoints()
+
+
+def main(args):
+    # create a subscriber instance
+    rospy.init_node('SGNetPredictor', anonymous=True)
+    sub = SGNetSubscriber(args)
+
+    # initializing the subscriber node
+    rospy.on_shutdown(sub.on_shutdown)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        print('Cuda:', torch.cuda.is_available())
+        main(parse_sgnet_args())
+    except rospy.ROSInterruptException:
+        pass
---
+++ nodes/detection/prediction/SGNet_utils.py
@@ -0,0 +1,289 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import argparse
+from torch.utils import data
+
+def parse_base_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--checkpoint', default='SGNet/SGNet_eth_checkpoint.pth', type=str)
+    parser.add_argument('--start_epoch', default=1, type=int)
+    parser.add_argument('--gpu', default='0', type=str)
+    parser.add_argument('--num_workers', default=8, type=int)
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--weight_decay', default=5e-04, type=float)
+    parser.add_argument('--seed', default=1, type=int)
+    parser.add_argument('--phases', default=['train', 'test'], type=list)
+    parser.add_argument('--shuffle', default=True, type=bool)
+
+    return parser
+
+
+def parse_sgnet_args():
+    parser = parse_base_args()
+    parser.add_argument('--dataset', default='ETH', type=str)
+    parser.add_argument('--lr', default=5e-04, type=float) # ETH 0.0005，HOTEL 0.0001, UNIV 0.0001, ZARA1 0.0001, ZARA2 0.0001
+    parser.add_argument('--eth_root', default='data/ETHUCY', type=str)
+    parser.add_argument('--model', default='SGNet_CVAE', type=str)
+    parser.add_argument('--hidden_size', default=512, type=int)
+    parser.add_argument('--enc_steps', default=8, type=int)
+    parser.add_argument('--dec_steps', default=12, type=int)
+    parser.add_argument('--dropout', default=0.5, type=float)
+    parser.add_argument('--nu', default=0.0, type=float)
+    parser.add_argument('--sigma', default=1.0, type=float)
+    parser.add_argument('--ETH_CONFIG', default='SGNet_ETH_UCY.json', type=str)
+    parser.add_argument('--augment', default=False, type=bool)
+    parser.add_argument('--DEC_WITH_Z', default=True, type=bool)
+    parser.add_argument('--LATENT_DIM', default=32, type=int)
+    parser.add_argument('--pred_dim', default=2, type=int)
+    parser.add_argument('--input_dim', default=6, type=int)
+    parser.add_argument('--K', default=20, type=int)
+
+    args, _ = parser.parse_known_args()
+    return args
+
+
+class ETHUCYFeatureExtractor(nn.Module):
+
+    def __init__(self, args):
+        super(ETHUCYFeatureExtractor, self).__init__()
+        self.embbed_size = args.hidden_size
+        self.embed = nn.Sequential(nn.Linear(6, self.embbed_size),
+                                        nn.ReLU())
+
+
+    def forward(self, inputs):
+        box_input = inputs
+
+        embedded_box_input= self.embed(box_input)
+
+        return embedded_box_input
+
+
+class SGNet(nn.Module):
+    def __init__(self, args):
+        super(SGNet, self).__init__()
+
+        self.hidden_size = args.hidden_size
+        self.enc_steps = args.enc_steps
+        self.dec_steps = args.dec_steps
+        self.dataset = args.dataset
+        self.dropout = args.dropout
+        self.feature_extractor = ETHUCYFeatureExtractor(args)
+        if self.dataset in ['JAAD', 'PIE']:
+            self.pred_dim = 4
+            self.regressor = nn.Sequential(nn.Linear(self.hidden_size,
+                                                     self.pred_dim),
+                                           nn.Tanh())
+            self.flow_enc_cell = nn.GRUCell(self.hidden_size * 2, self.hidden_size)
+        elif self.dataset in ['ETH', 'HOTEL', 'UNIV', 'ZARA1', 'ZARA2']:
+            self.pred_dim = 2
+            self.regressor = nn.Sequential(nn.Linear(self.hidden_size,
+                                                     self.pred_dim))
+
+        self.enc_goal_attn = nn.Sequential(nn.Linear(self.hidden_size // 4,
+                                                     1),
+                                           nn.ReLU(inplace=True))
+        self.dec_goal_attn = nn.Sequential(nn.Linear(self.hidden_size // 4,
+                                                     1),
+                                           nn.ReLU(inplace=True))
+
+        self.enc_to_goal_hidden = nn.Sequential(nn.Linear(self.hidden_size,
+                                                          self.hidden_size // 4),
+                                                nn.ReLU(inplace=True))
+        self.enc_to_dec_hidden = nn.Sequential(nn.Linear(self.hidden_size,
+                                                         self.hidden_size),
+                                               nn.ReLU(inplace=True))
+
+        self.goal_hidden_to_input = nn.Sequential(nn.Linear(self.hidden_size // 4,
+                                                            self.hidden_size // 4),
+                                                  nn.ReLU(inplace=True))
+        self.dec_hidden_to_input = nn.Sequential(nn.Linear(self.hidden_size,
+                                                           self.hidden_size),
+                                                 nn.ReLU(inplace=True))
+        self.goal_hidden_to_traj = nn.Sequential(nn.Linear(self.hidden_size // 4,
+                                                           self.hidden_size),
+                                                 nn.ReLU(inplace=True))
+        self.goal_to_enc = nn.Sequential(nn.Linear(self.hidden_size // 4,
+                                                   self.hidden_size // 4),
+                                         nn.ReLU(inplace=True))
+        self.goal_to_dec = nn.Sequential(nn.Linear(self.hidden_size // 4,
+                                                   self.hidden_size // 4),
+                                         nn.ReLU(inplace=True))
+        self.enc_drop = nn.Dropout(self.dropout)
+        self.goal_drop = nn.Dropout(self.dropout)
+        self.dec_drop = nn.Dropout(self.dropout)
+
+        self.traj_enc_cell = nn.GRUCell(self.hidden_size + self.hidden_size // 4, self.hidden_size)
+        self.goal_cell = nn.GRUCell(self.hidden_size // 4, self.hidden_size // 4)
+        self.dec_cell = nn.GRUCell(self.hidden_size + self.hidden_size // 4, self.hidden_size)
+
+    def SGE(self, goal_hidden):
+        goal_input = goal_hidden.new_zeros((goal_hidden.size(0), self.hidden_size // 4))
+        goal_traj = goal_hidden.new_zeros(goal_hidden.size(0), self.dec_steps, self.pred_dim)
+        goal_list = []
+        for dec_step in range(self.dec_steps):
+            goal_hidden = self.goal_cell(self.goal_drop(goal_input), goal_hidden)
+            goal_input = self.goal_hidden_to_input(goal_hidden)
+            goal_list.append(goal_hidden)
+            goal_traj_hidden = self.goal_hidden_to_traj(goal_hidden)
+            # regress goal traj for loss
+            goal_traj[:, dec_step, :] = self.regressor(goal_traj_hidden)
+        # get goal for decoder and encoder
+        goal_for_dec = [self.goal_to_dec(goal) for goal in goal_list]
+        goal_for_enc = torch.stack([self.goal_to_enc(goal) for goal in goal_list], dim=1)
+        enc_attn = self.enc_goal_attn(torch.tanh(goal_for_enc)).squeeze(-1)
+        enc_attn = F.softmax(enc_attn, dim=1).unsqueeze(1)
+        goal_for_enc = torch.bmm(enc_attn, goal_for_enc).squeeze(1)
+        return goal_for_dec, goal_for_enc, goal_traj
+
+    def decoder(self, dec_hidden, goal_for_dec):
+        # initial trajectory tensor
+        dec_traj = dec_hidden.new_zeros(dec_hidden.size(0), self.dec_steps, self.pred_dim)
+        for dec_step in range(self.dec_steps):
+            goal_dec_input = dec_hidden.new_zeros(dec_hidden.size(0), self.dec_steps, self.hidden_size // 4)
+            goal_dec_input_temp = torch.stack(goal_for_dec[dec_step:], dim=1)
+            goal_dec_input[:, dec_step:, :] = goal_dec_input_temp
+            dec_attn = self.dec_goal_attn(torch.tanh(goal_dec_input)).squeeze(-1)
+            dec_attn = F.softmax(dec_attn, dim=1).unsqueeze(1)
+            goal_dec_input = torch.bmm(dec_attn, goal_dec_input).squeeze(
+                1)  # .view(goal_hidden.size(0), self.dec_steps, self.hidden_size//4).sum(1)
+
+            dec_dec_input = self.dec_hidden_to_input(dec_hidden)
+            dec_input = self.dec_drop(torch.cat((goal_dec_input, dec_dec_input), dim=-1))
+            dec_hidden = self.dec_cell(dec_input, dec_hidden)
+            # regress dec traj for loss
+            dec_traj[:, dec_step, :] = self.regressor(dec_hidden)
+        return dec_traj
+
+    def encoder(self, traj_input, flow_input=None, start_index=0):
+        # initial output tensor
+        all_goal_traj = traj_input.new_zeros(traj_input.size(0), self.enc_steps, self.dec_steps, self.pred_dim)
+        all_dec_traj = traj_input.new_zeros(traj_input.size(0), self.enc_steps, self.dec_steps, self.pred_dim)
+        # initial encoder goal with zeros
+        goal_for_enc = traj_input.new_zeros((traj_input.size(0), self.hidden_size // 4))
+        # initial encoder hidden with zeros
+        traj_enc_hidden = traj_input.new_zeros((traj_input.size(0), self.hidden_size))
+        for enc_step in range(start_index, self.enc_steps):
+
+            traj_enc_hidden = self.traj_enc_cell(
+                self.enc_drop(torch.cat((traj_input[:, enc_step, :], goal_for_enc), 1)), traj_enc_hidden)
+            if self.dataset in ['JAAD', 'PIE', 'ETH', 'HOTEL', 'UNIV', 'ZARA1', 'ZARA2']:
+                enc_hidden = traj_enc_hidden
+            # generate hidden states for goal and decoder
+            goal_hidden = self.enc_to_goal_hidden(enc_hidden)
+            dec_hidden = self.enc_to_dec_hidden(enc_hidden)
+
+            goal_for_dec, goal_for_enc, goal_traj = self.SGE(goal_hidden)
+            dec_traj = self.decoder(dec_hidden, goal_for_dec)
+
+            # output
+            all_goal_traj[:, enc_step, :, :] = goal_traj
+            all_dec_traj[:, enc_step, :, :] = dec_traj
+
+        return all_goal_traj, all_dec_traj
+
+    def forward(self, inputs, start_index=0):
+        if self.dataset in ['JAAD', 'PIE']:
+            traj_input = self.feature_extractor(inputs)
+            all_goal_traj, all_dec_traj = self.encoder(traj_input)
+            return all_goal_traj, all_dec_traj
+        elif self.dataset in ['ETH', 'HOTEL', 'UNIV', 'ZARA1', 'ZARA2']:
+            traj_input_temp = self.feature_extractor(inputs[:, start_index:, :])
+            traj_input = traj_input_temp.new_zeros((inputs.size(0), inputs.size(1), traj_input_temp.size(-1)))
+            traj_input[:, start_index:, :] = traj_input_temp
+            all_goal_traj, all_dec_traj = self.encoder(traj_input, None, start_index)
+            return all_goal_traj, all_dec_traj
+
+
+def sgnet_iter(dataset, model, device, n=5):
+
+    dataloader = data.DataLoader(
+        dataset, batch_size=len(dataset), shuffle=False, num_workers=0)
+
+    # total_goal_loss = 0
+    # total_dec_loss = 0
+    # ADE_08 = 0
+    # ADE_12 = 0
+    # FDE_08 = 0
+    # FDE_12 = 0
+    # count = 0
+    model.eval()
+    # loader = tqdm(test_gen, total=len(test_gen))
+    with torch.set_grad_enabled(False):
+        for batch_idx, traj in enumerate(dataloader):
+
+            # first_history_index = torch.DoubleTensor([0]).to(device)
+            # print(first_history_index)
+            # assert torch.unique(first_history_index).shape[0] == 1
+            # batch_size = traj['input_x'].shape[0]
+            # count += batch_size
+            shift = traj[:, -1, :2].to('cpu').numpy()
+            # print(traj[:, :, :2] - traj[:, -1:, :2], (traj[:, :, :2] - traj[:, -1:, :2]).shape)
+            input_normalized = torch.cat([traj[:, :, :2] - traj[:, -1:, :2], traj[:, :, 2:]], dim=2)
+            # print(input_normalized, input_normalized.shape)
+            input_traj_torch = torch.DoubleTensor(input_normalized).to(device)
+            # print(input_traj_torch, input_traj_torch.shape)
+            # input_bbox_st = data['input_x_st'].to(device)
+            # target_traj = traj['target_y'].to(device)
+            # target_bbox_st = data['target_y_st'].to(device)
+            # all_guesses = []
+
+            # for j in range(n):
+            #     all_goal_traj, all_dec_traj = model(input_traj_torch)
+            #     all_dec_traj_np = all_dec_traj.to('cpu').numpy()
+            #     all_dec_traj_np = all_dec_traj_np[:, -1, -1, :]
+            #     # print(all_dec_traj_np)
+            #     all_dec_traj_np = all_dec_traj_np + shift
+            #     all_guesses.append(all_dec_traj_np)
+            # print(all_guesses)
+            all_goal_traj, all_dec_traj = model(input_traj_torch)
+            all_dec_traj_np = all_dec_traj.to('cpu').numpy()
+            all_dec_traj_np = all_dec_traj_np[:, -1:, -1, :]
+            all_dec_traj_np = np.swapaxes(all_dec_traj_np, 0, 1)
+            # print(all_dec_traj_np)
+            for i in range(len(all_dec_traj_np)):
+                all_dec_traj_np[i] = all_dec_traj_np[i] + shift
+            # print(all_dec_traj_np.shape)
+            # print(all_goal_traj, all_goal_traj.shape)
+            # print(all_dec_traj, all_dec_traj.shape)
+            # goal_loss = criterion(all_goal_traj[:, first_history_index[0]:, :, :],
+            #                       target_traj[:, first_history_index[0]:, :, :])
+            # dec_loss = criterion(all_dec_traj[:, first_history_index[0]:, :, :],
+            #                      target_traj[:, first_history_index[0]:, :, :])
+
+            # train_loss = goal_loss + dec_loss
+            #
+            # total_goal_loss += goal_loss.item() * batch_size
+            # total_dec_loss += dec_loss.item() * batch_size
+
+
+            # target_traj_np = target_traj.to('cpu').numpy()
+
+            # Decoder
+            # batch_ADE_08, batch_FDE_08, batch_ADE_12, batch_FDE_12 = \
+            #     eval_ethucy(input_traj_np, target_traj_np, all_dec_traj_np)
+
+
+    # print("ADE_08: %4f;  FDE_08: %4f;  ADE_12: %4f;   FDE_12: %4f\n" % (ADE_08, FDE_08, ADE_12, FDE_12))
+
+    return all_dec_traj_np
+
+
+class SGNetDatasetInit(data.Dataset):
+    def __init__(self, detected_object_trajs, velocity_objects, end_points, pad_past=8, pad_future=0, inference_timer_duration=0.5):
+        self.traj_flat = np.array([np.pad(np.array(traj), ((pad_past, pad_future), (0, 0)),
+                                     mode='edge')[end_points[i]:end_points[i] + pad_past + pad_future] for i, traj in enumerate(detected_object_trajs)])
+        self.velocitytraj = np.array([np.pad(np.array(traj), ((pad_past, pad_future), (0, 0)),
+                                     mode='constant')[end_points[i]:end_points[i] + pad_past + pad_future] for i, traj in enumerate(velocity_objects)]) / inference_timer_duration
+        self.acceltraj = np.zeros_like(self.velocitytraj)
+
+        self.traj = np.concatenate((self.traj_flat, self.velocitytraj, self.acceltraj), axis=2)
+
+    def __len__(self):
+        return len(self.traj)
+
+    def __getitem__(self, idx):
+        return self.traj[idx]
