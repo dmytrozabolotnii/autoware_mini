c44475fa3cc0d0294e114652a7d0077e8002dc7b
dmytro <dmytro.zabolotnii@ut.ee>
2024-03-04 17:52:16 +0200

    Forward header at time of inference in the candidate trajectories lanes for metrics module

--- nodes/detection/prediction/message_cache.py
+++ nodes/detection/prediction/message_cache.py
@@ -3,13 +3,14 @@
 class MessageCache:
     def __init__(self, id,
                  initial_trajectory_point,
-                 initial_velocity):
+                 initial_velocity, initial_header):
         self.id = id
         self.endpoints_count = 0
         self.raw_trajectories = [initial_trajectory_point]
         self.raw_velocities = [initial_velocity]
         self.prediction_history = [[]]
         self.predictions_history_danger_value = []
+        self.predictions_history_headers = [initial_header]
 
     def backpropagate_trajectories(self, pad_past=8):
         # Create fake history of movement depending on velocity vector we receive
@@ -18,12 +19,16 @@ class MessageCache:
         self.raw_velocities = [self.raw_velocities[0]] * pad_past + self.raw_velocities
         self.endpoints_count = pad_past + 1
 
-    def update_last_trajectory_velocity(self, trajectory, velocity):
+    def update_last_trajectory_velocity(self, trajectory, velocity, header):
         self.raw_trajectories[len(self.raw_trajectories) - 1] = trajectory
         self.raw_velocities[len(self.raw_velocities) - 1] = velocity
+        self.predictions_history_headers[len(self.predictions_history_headers) - 1] = header
 
     def return_last_prediction(self):
         return self.prediction_history[len(self.prediction_history) - 1]
+
+    def return_last_header(self):
+        return self.predictions_history_headers[len(self.predictions_history_headers) - 1]
 
     def extend_prediction_history(self, prediction):
         self.prediction_history.append(list(prediction))
@@ -35,3 +40,5 @@ class MessageCache:
         self.endpoints_count += 1
         self.raw_trajectories.append(self.raw_trajectories[len(self.raw_trajectories) - 1])
         self.raw_velocities.append(self.raw_velocities[len(self.raw_velocities) - 1])
+        self.predictions_history_headers.append(self.predictions_history_headers
+                                                [len(self.predictions_history_headers) - 1])
