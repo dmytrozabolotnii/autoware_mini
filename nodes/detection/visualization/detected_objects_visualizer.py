89c3dac2fd4143ea2ff2cd48b62afa0da410e82a
dmytro <dmytro.zabolotnii@ut.ee>
2024-06-10 18:00:57 +0300

    Combine metrics and prediction node, gatraj fix for less than 20 preds

--- nodes/detection/visualization/detected_objects_visualizer.py
+++ nodes/detection/visualization/detected_objects_visualizer.py
@@ -168,7 +168,7 @@ class DetectedObjectsVisualizer:
             marker.action = marker.DELETE
             markers.markers.append(marker)
 
-            for i in range(10):
+            for i in range(20):
                 marker = Marker(header=header)
                 marker.ns = 'candidate_trajectories_' + str(i)
                 marker.id = id
