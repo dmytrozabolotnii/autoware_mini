9ffc14596572524586f22cd6a18bb9987d9cba12
Mahir Gulzar <mahirgulzar@gmail.com>
2024-06-18 02:00:42 +0500

    [#110] setting --output argument via route scenario script because the default output param is not set in scenario runner fork

--- scripts/carla/carla_route_scenario.sh
+++ scripts/carla/carla_route_scenario.sh
@@ -6,4 +6,4 @@ scenarios_file=$2          # <AUTOWARE_MINI_PKG)/data/routes/all_towns_traffic_s
 route_id=$3                # 0 for tartu_demo map
 agent_file=$4              # <AUTOWARE_MINI_PKG)/nodes/platform/carla/carla_minimal_agent.py
 
-$SCENARIO_RUNNER_ROOT/scenario_runner.py --route $routes_file $scenarios_file $route_id --agent $agent_file --sync --waitForEgo
+$SCENARIO_RUNNER_ROOT/scenario_runner.py --route $routes_file $scenarios_file $route_id --agent $agent_file --sync --waitForEgo --output
