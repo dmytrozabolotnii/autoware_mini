c3620f414ba176bd752b5c5418c8e6e52f4821d1
Edgar Sepp <edgar.sepp@ut.ee>
2024-09-05 14:33:14 +0300

    #47 updated speed_planner and removed debug printouts

--- nodes/planning/local/speed_planner.py
+++ nodes/planning/local/speed_planner.py
@@ -4,7 +4,7 @@ import rospy
 import math
 import numpy as np
 from ros_numpy import numpify
-from shapely.geometry import Polygon, LineString, Point as ShapelyPoint
+from shapely.geometry import Point as ShapelyPoint
 from autoware_msgs.msg import Lane
 from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PoseStamped, TwistStamped, Vector3
@@ -51,10 +51,11 @@ class SpeedPlanner:
 
         if current_speed is None or current_position is None or collision_points is None:
             self.publish_local_path_wp([], msg.header)
+            rospy.logwarn_throttle(1, "%s - current speed, position or collision points not received!", rospy.get_name())
             return
 
         if  len(msg.waypoints) == 0 or len(collision_points) == 0:
-            # 0 length path or alterations from collision points are not possible
+            # no local path or no collision points menas no alterations to the path (publish msg)
             self.local_path_pub.publish(msg)
             return
 
@@ -63,46 +64,35 @@ class SpeedPlanner:
         local_path_blocked = False
         stopping_point_distance = 0.0
 
-        print("Collision points: ", collision_points)
-
-        # START here
+        # create local path
         local_path = Path(msg.waypoints)
         ego_distance_from_local_path_start = local_path.linestring.project(current_position)
-        collision_points_shapely_points = [ShapelyPoint(x, y, z) for x, y, z, vx, vy, vz, distance_to_stop, category in collision_points]
 
-        object_distances = [local_path.linestring.project(point) for point in collision_points_shapely_points]
-        print("Object distances: ", object_distances)
+        # extract object distances, velocities and braking distances
+        collision_points_shapely = [ShapelyPoint(x, y, z) for x, y, z, vx, vy, vz, distance_to_stop, category in collision_points]
+        object_distances = np.array([local_path.linestring.project(point) for point in collision_points_shapely])
         collision_points_path_headings = [local_path.get_heading_at_distance(distance) for distance in object_distances]
-        # TODO verify object_speeds
-        object_velocities = [project_vector_to_heading(heading, Vector3(vx, vy, vz))
-                             for heading, (x, y, z, vx, vy, vz, distance_to_stop, category) 
-                             in zip(collision_points_path_headings, collision_points)]
-        print("Object speeds: ", object_velocities)
-        
-        # extract distance_to_stop from collision_points
-        object_braking_distances = [distance_to_stop for x, y, z, vx, vy, vz, distance_to_stop, category in collision_points]
-        print("Distances to stop: ", object_braking_distances)
+        object_velocities = np.array([project_vector_to_heading(heading, Vector3(vx, vy, vz)) 
+                                      for heading, (x, y, z, vx, vy, vz, distance_to_stop, category)
+                                      in zip(collision_points_path_headings, collision_points)])
+        object_braking_distances = np.array([distance_to_stop for x, y, z, vx, vy, vz, distance_to_stop, category in collision_points])
 
-        # TODO separate check for TFL stopline and other objects - should not exceed deceleration limit
-
-        object_distances = np.array(object_distances)
-        object_velocities = np.array(object_velocities)
-        object_braking_distances = np.array(object_braking_distances)
-
-        # calculate target velocity for every object to select min target velocity object
+        # calculate target velocity for every collision pont
         target_distances = object_distances - ego_distance_from_local_path_start - self.current_pose_to_car_front - object_braking_distances - self.braking_reaction_time * np.abs(object_velocities)
         target_velocities = np.sqrt(np.maximum(0.0, np.maximum(0.0, object_velocities)**2 + 2 * self.default_deceleration * target_distances))
 
-        # index of min target velocity object
+        # find the closest collision point
         min_value_index = np.argmin(target_velocities)
-
         closest_object_distance = object_distances[min_value_index] - ego_distance_from_local_path_start - self.current_pose_to_car_front
         closest_object_velocity = object_velocities[min_value_index]
         stopping_point_distance = object_distances[min_value_index] - object_braking_distances[min_value_index]
+        # category 0 is goal point, not blocking
+        if collision_points[min_value_index]["category"] > 0:
+            local_path_blocked = True
 
-        # Recalculate target_velocity and overwrite in the waypoints
+        # Recalculate target_velocity for all the waypoints using the closest object
         zero_speeds_onwards = False
-        target_distance_obj = stopping_point_distance - self.current_pose_to_car_front - self.braking_reaction_time * np.abs(closest_object_velocity)
+        target_distance_object = stopping_point_distance - self.current_pose_to_car_front - self.braking_reaction_time * np.abs(closest_object_velocity)
         for i, wp in enumerate(local_path.waypoints):
 
             # once we get zero speed, keep it that way
@@ -111,17 +101,15 @@ class SpeedPlanner:
                 continue
 
             if i > 0:
-                target_distance_obj -= get_distance_between_two_points_2d(local_path.waypoints[i-1].pose.pose.position, local_path.waypoints[i].pose.pose.position)
-            target_velocity_obj = np.sqrt(np.maximum(0.0, np.maximum(0.0, closest_object_velocity)**2 + 2 * self.default_deceleration * target_distance_obj))
+                target_distance_object -= get_distance_between_two_points_2d(local_path.waypoints[i-1].pose.pose.position, local_path.waypoints[i].pose.pose.position)
+            target_velocity_object = np.sqrt(np.maximum(0.0, np.maximum(0.0, closest_object_velocity)**2 + 2 * self.default_deceleration * target_distance_object))
 
             # overwrite target velocity of wp
-            wp.twist.twist.linear.x = min(target_velocity_obj, wp.twist.twist.linear.x)
+            wp.twist.twist.linear.x = min(target_velocity_object, wp.twist.twist.linear.x)
 
             # from stop point onwards all speeds are set to zero
             if math.isclose(wp.twist.twist.linear.x, 0.0):
                 zero_speeds_onwards = True
-
-
 
         # Update the lane message with the calculated values
         self.publish_local_path_wp(local_path.waypoints, msg.header, closest_object_distance, closest_object_velocity, local_path_blocked, stopping_point_distance)
