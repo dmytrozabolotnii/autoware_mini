d595cf6d29e71e85daca056f6e645b576729771f
Edgar Sepp <edgar.sepp@ut.ee>
2024-09-05 15:05:08 +0300

    #47 added red stopline deceleration check to traffic_light_stopline

--- nodes/planning/local/traffic_light_stopline.py
+++ nodes/planning/local/traffic_light_stopline.py
@@ -3,6 +3,7 @@
 import rospy
 from shapely.geometry import Point as ShapelyPoint
 from autoware_msgs.msg import Lane, TrafficLightResultArray
+from geometry_msgs.msg import PoseStamped, TwistStamped
 from sensor_msgs.msg import PointCloud2
 from helpers.path import Path
 from helpers.collision import CollisionPoints
@@ -14,6 +15,9 @@ class TrafficLightStopline:
 
         # parameters
         self.braking_safety_distance_stopline = rospy.get_param("~braking_safety_distance_stopline")
+        self.tfl_force_stop_speed_limit = rospy.get_param("~tfl_force_stop_speed_limit")
+        self.tfl_maximum_deceleration = rospy.get_param("~tfl_maximum_deceleration")
+        self.current_pose_to_car_front = rospy.get_param("current_pose_to_car_front")
         coordinate_transformer = rospy.get_param("/localization/coordinate_transformer")
         use_custom_origin = rospy.get_param("/localization/use_custom_origin")
         utm_origin_lat = rospy.get_param("/localization/utm_origin_lat")
@@ -22,17 +26,26 @@ class TrafficLightStopline:
 
         # variables
         self.stopline_statuses = {}
+        self.current_position = None
+        self.current_speed = None
 
         # publishers
         self.traffic_light_stopline_pub = rospy.Publisher('collision_tfl_stopline', PointCloud2, queue_size=1, tcp_nodelay=True)
 
         # subscribers
+        rospy.Subscriber('/localization/current_pose', PoseStamped, self.current_pose_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('/localization/current_velocity', TwistStamped, self.current_velocity_callback, queue_size=1, tcp_nodelay=True)
         rospy.Subscriber('extracted_local_path', Lane, self.path_callback, queue_size=1, tcp_nodelay=True)
         rospy.Subscriber('/detection/traffic_light_status', TrafficLightResultArray, self.traffic_light_status_callback, queue_size=1, tcp_nodelay=True)
 
         lanelet2_map = load_lanelet2_map(lanelet2_map_name, coordinate_transformer, use_custom_origin, utm_origin_lat, utm_origin_lon)
         self.all_stoplines = get_stoplines(lanelet2_map)
 
+    def current_pose_callback(self, msg):
+        self.current_position = ShapelyPoint(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
+
+    def current_velocity_callback(self, msg):
+        self.current_speed = msg.twist.linear.x
 
     def traffic_light_status_callback(self, msg):
         stopline_statuses = {}
@@ -43,23 +56,37 @@ class TrafficLightStopline:
 
     def path_callback(self, msg):
 
+        current_position = self.current_position
+        current_speed = self.current_speed
+
+        if current_speed is None or current_position is None:
+            rospy.logwarn_throttle(1, "%s - current speed or position not received!", rospy.get_name())
+            return
+
         stopline_statuses = self.stopline_statuses
         collision_points = CollisionPoints()
 
         if len(msg.waypoints) == 0 or len(stopline_statuses) == 0:
+            # no local path or no stoplines - create empty collision points message
             collision_points_msg = collision_points.get_message()
         else:
             local_path = Path(msg.waypoints)
+            ego_distance_from_local_path_start = local_path.linestring.project(current_position)
 
             for stopline_id, stopline_linestring in self.all_stoplines.items():
                 # if RED and intersects with local path
                 if stopline_id in stopline_statuses and stopline_statuses[stopline_id] == 0 and stopline_linestring.intersects(local_path.linestring):
                     intersection_point = local_path.linestring.intersection(stopline_linestring)
                     assert isinstance(intersection_point, ShapelyPoint), "Stop line and local path intersection point is not a ShapelyPoint"
-                    x, y, z = intersection_point.x, intersection_point.y, intersection_point.z
-
-                    # TODO last field category !?!?  1 - TFL stopline
-                    collision_points.add_point(x, y, z, 0.0, 0.0, 0.0, self.braking_safety_distance_stopline, 1)
+                    
+                    # check deceleration
+                    distance_to_stopline = local_path.linestring.project(intersection_point)
+                    distance_for_deceleration = distance_to_stopline - ego_distance_from_local_path_start - self.current_pose_to_car_front
+                    deceleration = (current_speed**2) / (2 * distance_for_deceleration)
+                    # base_link has not crossed the stopline and velocity is below tfl_force_stop_speed_limit or deceleration is less than maximum allowed deceleration
+                    if distance_to_stopline > 0 and current_speed < self.tfl_force_stop_speed_limit / 3.6 or 0 <= deceleration <= self.tfl_maximum_deceleration:
+                        x, y, z = intersection_point.x, intersection_point.y, intersection_point.z
+                        collision_points.add_point(x, y, z, 0.0, 0.0, 0.0, self.braking_safety_distance_stopline, 1)
 
             collision_points_msg = collision_points.get_message()
 
