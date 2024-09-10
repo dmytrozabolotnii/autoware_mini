18b6e84bff78cffc72fe2814b5c4524571b388e0
Edgar Sepp <edgar.sepp@ut.ee>
2024-09-10 17:40:45 +0300

    #47 !61 fix goal collision point, add only when on local_path

--- nodes/planning/local/stop_at_goal.py
+++ nodes/planning/local/stop_at_goal.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import rospy
+import math
 from autoware_msgs.msg import Lane
 from sensor_msgs.msg import PointCloud2
 from helpers.collision import CollisionPoints, CAT_GOAL_POINT
+from helpers.geometry import get_distance_between_two_points_2d
 
 class StopAtGoal:
 
@@ -12,27 +14,34 @@ class StopAtGoal:
         # parameters
         self.braking_safety_distance_goal = rospy.get_param("~braking_safety_distance_goal")
 
+        # variables
+        self.goal_point = None
+
         # publishers
         self.goal_point_pub = rospy.Publisher('collision_goal', PointCloud2, queue_size=1, latch=True, tcp_nodelay=True)
 
         # subscribers
-        rospy.Subscriber('smoothed_path', Lane, self.path_callback, queue_size=None, tcp_nodelay=True)
+        rospy.Subscriber('extracted_local_path', Lane, self.local_path_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('smoothed_path', Lane, self.global_path_callback, queue_size=1, tcp_nodelay=True)
 
-    def path_callback(self, msg):
-
-        collision_points = CollisionPoints()
+    def global_path_callback(self, msg):
 
         if len(msg.waypoints) > 0:
-            # Extract last point from the path
-            last_point = msg.waypoints[-1]
+            # lasst point of the global path is goal point
+            self.goal_point = msg.waypoints[-1].pose.pose.position
+        else:
+            self.goal_point = None
 
-            # Extract x, y, z coordinates
-            x = last_point.pose.pose.position.x
-            y = last_point.pose.pose.position.y
-            z = last_point.pose.pose.position.z
+    def local_path_callback(self, msg):
 
-            # Create goal point
-            collision_points.add_point(x, y, z, 0.0, 0.0, 0.0, self.braking_safety_distance_goal, CAT_GOAL_POINT)
+        collision_points = CollisionPoints()
+        goal_point = self.goal_point
+
+        if goal_point is not None:
+            # check if goal point is at the end of the local path
+            if math.isclose(get_distance_between_two_points_2d(goal_point, msg.waypoints[-1].pose.pose.position), 0.0):
+                # add goal point as collision point
+                collision_points.add_point(goal_point.x, goal_point.y, goal_point.z, 0.0, 0.0, 0.0, self.braking_safety_distance_goal, CAT_GOAL_POINT)
 
         collision_points_msg = collision_points.create_message()
         collision_points_msg.header.frame_id = msg.header.frame_id
