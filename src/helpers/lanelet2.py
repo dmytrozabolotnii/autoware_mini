613e4df6d293105cbc5c8d20bdfa8a301d8b8787
Karl-Johan Pilve <karl-johan.pilve@ut.ee>
2024-09-18 16:23:45 +0300

    Function for getting traffic light corners from the lanelet2 map

--- src/helpers/lanelet2.py
+++ src/helpers/lanelet2.py
@@ -80,6 +80,37 @@ def get_stoplines_api_id(lanelet2_map):
 
     return stopline_ids
 
+def get_stoplines_trafficlights(lanelet2_map):
+    """
+    Iterate over all regulatory_elements with subtype traffic light and extract the stoplines and sinals.
+    Organize the data into dictionary indexed by stopline id that contains a traffic_light id and the four coners of the traffic light.
+    :param lanelet2_map: lanelet2 map
+    :return: {stopline_id: {traffic_light_id: {'top_left': [x, y, z], 'top_right': [...], 'bottom_left': [...], 'bottom_right': [...]}, ...}, ...}
+    """
+
+    signals = {}
+
+    for reg_el in lanelet2_map.regulatoryElementLayer:
+        if reg_el.attributes["subtype"] == "traffic_light":
+            # ref_line is the stop line and there is only 1 stopline per traffic light reg_el
+            linkId = reg_el.parameters["ref_line"][0].id
+            
+            for tfl in reg_el.parameters["refers"]:
+                tfl_height = float(tfl.attributes["height"])
+                # plId represents the traffic light (pole), one stop line can be associated with multiple traffic lights
+                plId = tfl.id
+
+                traffic_light_data = {'top_left': [tfl[0].x, tfl[0].y, tfl[0].z + tfl_height], 
+                                      'top_right': [tfl[1].x, tfl[1].y, tfl[1].z + tfl_height], 
+                                      'bottom_left': [tfl[0].x, tfl[0].y, tfl[0].z], 
+                                      'bottom_right': [tfl[1].x, tfl[1].y, tfl[1].z]}
+
+
+                # signals is a dictionary indexed by stopline id and contains dictionary of traffic lights indexed by pole id
+                # which in turn contains a dictionary of traffic light corners
+                signals.setdefault(linkId, {}).setdefault(plId, traffic_light_data)
+
+    return signals
 
 def get_stoplines_trafficlights_bulbs(lanelet2_map):
     """
