65478420fca7b081af893e6bd97099be4c3a54e0
Edgar Sepp <edgar.sepp@ut.ee>
2024-11-26 11:18:16 +0200

    !81 comment out bounding_box marker creation and visualization

--- nodes/detection/visualization/detected_objects_visualizer.py
+++ nodes/detection/visualization/detected_objects_visualizer.py
@@ -41,25 +41,25 @@ class DetectedObjectsVisualizer:
             marker.color = obj.color
             markers.markers.append(marker)
             
-            # bounding box
-            marker = Marker(header=header)
-            marker.ns = 'bounding_box'
-            marker.id = obj.id
-            marker.type = marker.LINE_STRIP
-            marker.action = marker.ADD
-            marker.pose = obj.pose
-            marker.scale.x = 0.1
-            marker.color = ColorRGBA(1.0, 0.0, 0.0, 0.8)
-            half_length = obj.dimensions.x / 2.0
-            half_width = obj.dimensions.y / 2.0
-            marker.points = [
-                Point(-half_length, -half_width, 0.0),
-                Point(-half_length, half_width, 0.0),
-                Point(half_length, half_width, 0.0),
-                Point(half_length, -half_width, 0.0),
-                Point(-half_length, -half_width, 0.0),
-            ]
-            markers.markers.append(marker)
+            # # bounding box
+            # marker = Marker(header=header)
+            # marker.ns = 'bounding_box'
+            # marker.id = obj.id
+            # marker.type = marker.LINE_STRIP
+            # marker.action = marker.ADD
+            # marker.pose = obj.pose
+            # marker.scale.x = 0.1
+            # marker.color = ColorRGBA(1.0, 0.0, 0.0, 0.8)
+            # half_length = obj.dimensions.x / 2.0
+            # half_width = obj.dimensions.y / 2.0
+            # marker.points = [
+            #     Point(-half_length, -half_width, 0.0),
+            #     Point(-half_length, half_width, 0.0),
+            #     Point(half_length, half_width, 0.0),
+            #     Point(half_length, -half_width, 0.0),
+            #     Point(-half_length, -half_width, 0.0),
+            # ]
+            # markers.markers.append(marker)
 
             # convex hull
             if len(obj.convex_hull.points) > 0:
@@ -113,11 +113,11 @@ class DetectedObjectsVisualizer:
             marker.action = marker.DELETE
             markers.markers.append(marker)
 
-            marker = Marker(header=header)
-            marker.ns = 'bounding_box'
-            marker.id = id
-            marker.action = marker.DELETE
-            markers.markers.append(marker)
+            # marker = Marker(header=header)
+            # marker.ns = 'bounding_box'
+            # marker.id = id
+            # marker.action = marker.DELETE
+            # markers.markers.append(marker)
 
             marker = Marker(header=header)
             marker.ns = 'convex_hull'
