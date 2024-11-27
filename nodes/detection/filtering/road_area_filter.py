7661735e4e2b45558d7dc7c89bdd52286494fa41
Edgar Sepp <edgar.sepp@ut.ee>
2024-11-27 12:59:40 +0200

    !81 adjusted param to match ground removal filter and replaced Polygon with mutipoints

--- nodes/detection/filtering/road_area_filter.py
+++ nodes/detection/filtering/road_area_filter.py
@@ -116,7 +116,7 @@ class RoadAreaFilter:
             if self.filtering_method == "centroid":
                 obj_geom = shapely.Point(obj.pose.position.x, obj.pose.position.y)
             else:
-                obj_geom = shapely.Polygon([(p.x, p.y) for p in obj.convex_hull.points])
+                obj_geom = shapely.multipoints([(p.x, p.y) for p in obj.convex_hull.points])
             shapely.prepare(obj_geom)
 
             if self.filtering_method == "centroid" or self.filtering_method == "intersects":
