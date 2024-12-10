e397aee8e62d32e9e0c11deaaa693c2e7c4583dd
Edgar Sepp <edgar.sepp@ut.ee>
2024-12-10 15:24:23 +0200

    #179 made get_polygon_with use heading_angle in radians and unified the usage.

--- nodes/detection/visualization/predicted_trajectory_visualizer.py
+++ nodes/detection/visualization/predicted_trajectory_visualizer.py
@@ -46,7 +46,7 @@ class PredictedTrajectoryVisualizer:
                 marker.color = ColorRGBA(1.0, 1.0, 0.0, 0.5)
                 if self.use_object_width:
                     object_polygon = shapely.Polygon([(p.x, p.y) for p in obj.convex_hull.points])
-                    object_heading = math.degrees(math.atan2(obj.velocity.y, obj.velocity.x))
+                    object_heading = math.atan2(obj.velocity.y, obj.velocity.x)
                     marker.scale.x = get_polygon_width(object_polygon, object_heading)
                 else:
                     marker.scale.x = 0.2
--- src/helpers/shapely.py
+++ src/helpers/shapely.py
@@ -1,16 +1,17 @@
 import shapely
+import math
 
 def get_polygon_width(polygon, heading_angle):
     """
     Get width of the polygon. Measured perpendicular to moving direction.
     :param polygon: shapely Polygon
-    :param heading_angle: heading angle in degrees
+    :param heading_angle: heading angle in radians
     :return: width of the polygon
     """
 
     # rotate polygon to align with y axis, so the width will be in x direction
-    angle = 90 - heading_angle
-    rotated_polygon = shapely.affinity.rotate(polygon, angle, origin='centroid', use_radians=False)
+    angle = math.pi/2 - heading_angle
+    rotated_polygon = shapely.affinity.rotate(polygon, angle, origin='centroid', use_radians=True)
     minx, miny, maxx, maxy = rotated_polygon.bounds
     width = maxx - minx
     return width
