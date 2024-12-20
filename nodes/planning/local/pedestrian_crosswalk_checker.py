cb91de824cd3d4f4c75b6466206389da79fdd2eb
Edgar Sepp <edgar.sepp@ut.ee>
2024-12-20 18:33:59 +0200

    #184 - change to find min angle between closest_intersection_on_path_heading and trajectory_heading_at_closest_intersection

--- nodes/planning/local/pedestrian_crosswalk_checker.py
+++ nodes/planning/local/pedestrian_crosswalk_checker.py
@@ -7,7 +7,7 @@ import shapely.ops
 from autoware_mini.msg import Path, DetectedObjectArray
 from sensor_msgs.msg import PointCloud2
 from tf2_ros import TransformListener, Buffer, TransformException
-from helpers.geometry import get_vector_norm_3d, get_heading_from_vector, get_heading_between_two_points, get_angle_between_two_headings
+from helpers.geometry import get_vector_norm_3d, get_heading_from_vector, get_heading_between_two_points, get_angle_between_two_headings, get_minimum_angle_between_two_lines
 from helpers.collision import CollisionPoints
 from helpers.lanelet2 import load_lanelet2_map, get_crosswalks
 from helpers.shapely import get_polygon_width, get_heading_at_distance_along_linestring
@@ -134,9 +134,22 @@ class PedestrianCrosswalkChecker:
 
                                 if crosswalk['polygon'].intersects(trajectory):
                                     intersection_points = shapely.get_coordinates(crosswalk['polygon'].intersection(trajectory))
-                                    closest_distance_to_object = min([trajectory_linestring.project(shapely.Point(x, y)) for x, y in intersection_points])
-                                    trajectory_heading = get_heading_at_distance_along_linestring(trajectory_linestring, closest_distance_to_object)
-                                    if math.degrees(get_angle_between_two_headings(trajectory_heading, object_projection_on_path_heading)) < self.crossing_angle_max_limit:
+                                    closest_distance_to_object = float('inf')
+                                    closest_intersection_point = None
+                                    for x, y in intersection_points:
+                                        distance = trajectory_linestring.project(shapely.Point(x, y))
+                                        if distance < closest_distance_to_object:
+                                            closest_distance_to_object = distance
+                                            closest_intersection_point = (x, y)
+
+                                    closest_intersection_point = shapely.Point(closest_intersection_point)
+                                    trajectory_heading_at_closest_intersection = get_heading_at_distance_along_linestring(trajectory_linestring, closest_distance_to_object)
+                                    # find heading from the closest intersection point to its projection on local_path
+                                    closest_intersection_distance_from_local_path_start = local_path_linestring.project(closest_intersection_point)
+                                    closest_intersection_on_path = local_path_linestring.interpolate(closest_intersection_distance_from_local_path_start)
+                                    closest_intersection_on_path_heading = get_heading_between_two_points(closest_intersection_point, closest_intersection_on_path)
+
+                                    if math.degrees(get_minimum_angle_between_two_lines(trajectory_heading_at_closest_intersection, closest_intersection_on_path_heading)) < self.crossing_angle_max_limit:
                                         collision_points.add_intersection_points(crosswalk['intersection_points'], z=obj.position.z, vx=0, vy=0, vz=0, distance_to_stop=self.braking_safety_distance_crosswalk, category=CollisionPoints.TRAJECTORY_ON_CROSSWALK)
                                         crosswalks_on_local_path.remove(crosswalk)
                                         break
