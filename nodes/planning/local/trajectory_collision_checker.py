27bb2f85e40c89d7e47cfe01bba4cdab3f818808
Edgar Sepp <edgar.sepp@ut.ee>
2025-01-22 14:01:57 +0200

    !101 #194 remove redundand if, for loop does it already

--- nodes/planning/local/trajectory_collision_checker.py
+++ nodes/planning/local/trajectory_collision_checker.py
@@ -48,52 +48,51 @@ class TrajectoryCollisionChecker:
 
         for obj in detected_objects:
 
-            if len(obj.candidate_trajectories.paths) > 0:
-                for path in obj.candidate_trajectories.paths:
+            for path in obj.candidate_trajectories.paths:
 
-                    trajectory_to_check = PathWrapper(path.waypoints).linestring
+                trajectory_to_check = PathWrapper(path.waypoints).linestring
 
-                    if self.use_object_width:
-                        trajectory_to_check = trajectory_to_check.buffer(path.waypoints[0].left_width, cap_style="flat")
+                if self.use_object_width:
+                    trajectory_to_check = trajectory_to_check.buffer(path.waypoints[0].left_width, cap_style="flat")
 
-                    if local_path_buffer.intersects(trajectory_to_check):
-                        trajectory_intersection_result = trajectory_to_check.intersection(local_path_buffer)
-                        trajectory_intersection_points = shapely.get_coordinates(trajectory_intersection_result)
-                        trajectory_intersection_distance = min([local_path.linestring.project(shapely.Point(x, y)) for x, y in trajectory_intersection_points])
+                if local_path_buffer.intersects(trajectory_to_check):
+                    trajectory_intersection_result = trajectory_to_check.intersection(local_path_buffer)
+                    trajectory_intersection_points = shapely.get_coordinates(trajectory_intersection_result)
+                    trajectory_intersection_distance = min([local_path.linestring.project(shapely.Point(x, y)) for x, y in trajectory_intersection_points])
 
-                        object_current_location = shapely.Point(obj.position.x, obj.position.y)
-                        object_distance_from_local_path_start = local_path.linestring.project(object_current_location)
+                    object_current_location = shapely.Point(obj.position.x, obj.position.y)
+                    object_distance_from_local_path_start = local_path.linestring.project(object_current_location)
 
-                        # Ignore trajectories from behind
-                        if math.isclose(trajectory_intersection_distance, 0.0, abs_tol=0.001) and math.isclose(object_distance_from_local_path_start, 0.0, abs_tol=0.001):
-                            continue
+                    # Ignore trajectories from behind
+                    if math.isclose(trajectory_intersection_distance, 0.0, abs_tol=0.001) and math.isclose(object_distance_from_local_path_start, 0.0, abs_tol=0.001):
+                        continue
 
-                        object_current_heading = get_heading_from_vector(obj.velocity)
-                        object_local_path_heading = local_path.get_heading_at_distance(object_distance_from_local_path_start)
-                        heading_difference = math.degrees(get_angle_between_two_headings(object_current_heading, object_local_path_heading))
+                    object_current_heading = get_heading_from_vector(obj.velocity)
+                    object_local_path_heading = local_path.get_heading_at_distance(object_distance_from_local_path_start)
+                    heading_difference = math.degrees(get_angle_between_two_headings(object_current_heading, object_local_path_heading))
 
-                        # Ignore object trajectories that are on our path and with similar heading - must be in front of us
-                        if local_path_buffer.intersects(object_current_location) and heading_difference < self.heading_alignment_limit:
-                            continue
+                    # Ignore object trajectories that are on our path and with similar heading - must be in front of us
+                    if local_path_buffer.intersects(object_current_location) and heading_difference < self.heading_alignment_limit:
+                        continue
 
-                        if heading_difference < self.heading_alignment_limit:
-                            # objects with similar heading - add collision points with object's velocity
-                            collision_points.add_intersection_points(trajectory_intersection_points,
-                                                                    z = obj.position.z,
-                                                                    vx = obj.velocity.x,
-                                                                    vy = obj.velocity.y,
-                                                                    vz = obj.velocity.z,
-                                                                    distance_to_stop = self.braking_safety_distance_obstacle,
-                                                                    category = CollisionPoints.MERGING_TRAJECTORY)
-                        else:
-                            # objects intersecting at angle, add with 0 velocity
-                            collision_points.add_intersection_points(trajectory_intersection_points,
-                                    z = obj.position.z,
-                                    vx = 0,
-                                    vy = 0,
-                                    vz = 0,
-                                    distance_to_stop = self.braking_safety_distance_obstacle,
-                                    category = CollisionPoints.COLLIDING_TRAJECTORY)
+                    if heading_difference < self.heading_alignment_limit:
+                        # objects with similar heading - add collision points with object's velocity
+                        collision_points.add_intersection_points(trajectory_intersection_points,
+                                                                z = obj.position.z,
+                                                                vx = obj.velocity.x,
+                                                                vy = obj.velocity.y,
+                                                                vz = obj.velocity.z,
+                                                                distance_to_stop = self.braking_safety_distance_obstacle,
+                                                                category = CollisionPoints.MERGING_TRAJECTORY)
+                    else:
+                        # objects intersecting at angle, add with 0 velocity
+                        collision_points.add_intersection_points(trajectory_intersection_points,
+                                z = obj.position.z,
+                                vx = 0,
+                                vy = 0,
+                                vz = 0,
+                                distance_to_stop = self.braking_safety_distance_obstacle,
+                                category = CollisionPoints.COLLIDING_TRAJECTORY)
 
         collision_points_msg = collision_points.create_message()
         collision_points_msg.header = msg.header
