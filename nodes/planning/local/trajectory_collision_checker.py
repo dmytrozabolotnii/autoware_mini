970b5a2df69e2a0fc7121e172d0db3e47b316f70
Edgar Sepp <edgar.sepp@ut.ee>
2025-02-11 11:03:33 +0200

    !102 using multilinestrings and adding prepare

--- nodes/planning/local/trajectory_collision_checker.py
+++ nodes/planning/local/trajectory_collision_checker.py
@@ -38,7 +38,8 @@ class TrajectoryCollisionChecker:
 
         lanelet2_map = load_lanelet2_map(lanelet2_map_name)
         stop_lines = get_stop_lines_using_subtype(lanelet2_map, subtype=["stop", "traffic_light", "yield", "yield_stop"])
-        self.stop_lines = shapely.MultiLineString(list(stop_lines.values()))
+        self.stop_lines = shapely.multilinestrings(list(stop_lines.values()))
+        shapely.prepare(self.stop_lines)
 
         # publishers
         self.local_path_collision_pub = rospy.Publisher('trajectory_collision_points', PointCloud2, queue_size=1, tcp_nodelay=True)
