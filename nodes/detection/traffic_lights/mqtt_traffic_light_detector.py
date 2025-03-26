007da44919e10eb2f8fb21c3a2b53f8db18af852
Karl-Johan Pilve <karl-johan.pilve@ut.ee>
2025-03-26 11:22:17 +0200

    !124 since_change and till_change as optional parameters

--- nodes/detection/traffic_lights/mqtt_traffic_light_detector.py
+++ nodes/detection/traffic_lights/mqtt_traffic_light_detector.py
@@ -42,7 +42,9 @@ BINARY_MQTT_TO_STR = {
     6: "DARK"
 }
 
-BINARY_MQTT_MSG_FORMAT = "<B Q B i"  # Version, Timestamp, Status, Since Change
+BINARY_MQTT_MSG_FORMAT_1 = "<B Q B"  # Version, Timestamp, Status
+BINARY_MQTT_MSG_FORMAT_2 = "<B Q B i"  # Version, Timestamp, Status, Since Change
+BINARY_MQTT_MSG_FORMAT_3 = "<B Q B i i"  # Version, Timestamp, Status, Since Change, Till Change
 
 class MqttTrafficLightDetector:
     def __init__(self):
@@ -122,16 +124,30 @@ class MqttTrafficLightDetector:
         # if message starts with '{' then it's in json format
         if chr(msg.payload[0]) == "{":
             mqtt_data = json.loads(msg.payload)
+            
         else:
-            if len(msg.payload) != struct.calcsize(BINARY_MQTT_MSG_FORMAT):
-                rospy.logerr('%s - binary mqtt message size %d does not match expected size %d', rospy.get_name(), len(msg.payload), struct.calcsize(BINARY_MQTT_MSG_FORMAT))
+            if len(msg.payload) == struct.calcsize(BINARY_MQTT_MSG_FORMAT_1):
+                version, timestamp, status = struct.unpack(BINARY_MQTT_MSG_FORMAT_1, msg.payload)
+                since_change = -1
+                till_change = -1
+
+            elif len(msg.payload) == struct.calcsize(BINARY_MQTT_MSG_FORMAT_2):
+                version, timestamp, status, since_change = struct.unpack(BINARY_MQTT_MSG_FORMAT_2, msg.payload)
+                till_change = -1
+
+            elif len(msg.payload) == struct.calcsize(BINARY_MQTT_MSG_FORMAT_3):
+                version, timestamp, status, since_change, till_change = struct.unpack(BINARY_MQTT_MSG_FORMAT_3, msg.payload)
+                
+            else:
+                rospy.logerr('%s - binary mqtt message size %d does not match any expected sizes %d, %d or %d', rospy.get_name(), len(msg.payload), 
+                             struct.calcsize(BINARY_MQTT_MSG_FORMAT_1), struct.calcsize(BINARY_MQTT_MSG_FORMAT_2), struct.calcsize(BINARY_MQTT_MSG_FORMAT_3))
                 return
 
-            version, timestamp, status, since_change = struct.unpack(BINARY_MQTT_MSG_FORMAT, msg.payload)
             mqtt_data = {"version": version,
                          "timestamp": timestamp,
                          "status": BINARY_MQTT_TO_STR[status],
-                         "since_change": since_change}
+                         "since_change": since_change,
+                         "till_change": till_change}
 
         self.mqtt_status[api_id] = mqtt_data
 
