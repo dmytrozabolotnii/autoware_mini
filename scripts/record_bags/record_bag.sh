5b5a7ce55f73af7eda544c01232b9af4945c0cba
Karl-Johan Pilve <karl-johan.pilve@ut.ee>
2025-04-01 09:17:36 +0300

    !126 Update bag recording script

--- scripts/record_bags/record_bag.sh
+++ scripts/record_bags/record_bag.sh
@@ -2,7 +2,8 @@
 
 name=$1
 script_dir=$(realpath "$(dirname "$0")")
+blacklist_file=$(dirname "$(dirname "$script_dir")")/config/record_bag/blacklist.txt
 
 cd /media/$USER/ExtremePro/$USER
 
-rosbag record -a -O $(date +"%Y-%m-%d-%H-%M-%S")_$name -x "$(grep -v -P '^#(.*)' $script_dir/blacklist.txt | xargs | sed -e 's/ /|/g')"
+rosbag record -a -O $(date +"%Y-%m-%d-%H-%M-%S")_$name -x "$(grep -v -P '^#(.*)' $blacklist_file | xargs | sed -e 's/ /|/g')"
