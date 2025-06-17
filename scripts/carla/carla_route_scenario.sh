8fde9b18affdd473e34cd9ce9453f993cfc58428
Tambet Matiisen <tambet.matiisen@ut.ee>
2025-06-17 14:16:23 +0300

    Correctly passing Carla host and port to all subsystems to make possible to run Carla on server

--- scripts/carla/carla_route_scenario.sh
+++ scripts/carla/carla_route_scenario.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+host=$1                    # Carla host
+port=$2                    # Carla port
+routes_file=$3             # <AUTOWARE_MINI_PKG)/data/routes/tartu_demo.xml
+route_id=$4                # 0 for tartu_demo map
+agent_file=$5              # <AUTOWARE_MINI_PKG)/nodes/platform/carla/carla_minimal_agent.py
 
-routes_file=$1             # <AUTOWARE_MINI_PKG)/data/routes/routes_devtest.xml
-route_id=$2                # 0 for tartu_demo map
-agent_file=$3              # <AUTOWARE_MINI_PKG)/nodes/platform/carla/carla_minimal_agent.py
-
-$SCENARIO_RUNNER_ROOT/scenario_runner.py --route $routes_file --route-id $route_id --agent $agent_file --sync --waitForEgo --output --timeout 60
+$SCENARIO_RUNNER_ROOT/scenario_runner.py --route $routes_file --route-id $route_id --agent $agent_file --sync --waitForEgo --output --host $host --port $port --timeout 60
