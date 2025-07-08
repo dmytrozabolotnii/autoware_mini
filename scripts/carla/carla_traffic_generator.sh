090a884561680b167105b033502707c5ee036cba
Tambet Matiisen <tambet.matiisen@ut.ee>
2025-07-08 21:14:46 +0300

    Increased traffic generator delay to not fail with timeout

--- scripts/carla/carla_traffic_generator.sh
+++ scripts/carla/carla_traffic_generator.sh
@@ -4,7 +4,7 @@ host=$1                    # Carla host
 port=$2                    # Carla port
 
 # wait Carla to start
-sleep 15
+sleep 30
 
 # launch the script
 $CARLA_ROOT/PythonAPI/examples/generate_traffic.py --host $host --port $port --async
